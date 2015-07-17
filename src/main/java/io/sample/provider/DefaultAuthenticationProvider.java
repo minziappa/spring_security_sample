aeb64b35079ff4a3eca550f7fb126052113008a9
kim joon <minziappa@gmail.com>
2015-07-17 18:25:40 +0900

    Fixed bugs

--- src/main/java/io/sample/provider/DefaultAuthenticationProvider.java
+++ src/main/java/io/sample/provider/DefaultAuthenticationProvider.java
@@ -1,15 +1,21 @@
 package io.sample.provider;
 
+import io.sample.bean.ExtendUser;
+import io.sample.bean.model.UserModel;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
@@ -17,30 +23,63 @@ import org.springframework.stereotype.Component;
 public class DefaultAuthenticationProvider implements AuthenticationProvider {
 
 	final Logger logger = LoggerFactory.getLogger(DefaultAuthenticationProvider.class);
+	@Autowired
+	private Md5PasswordEncoder passwordEncoder;
 
 	@Override
 	public Authentication authenticate(Authentication authentication)
 		throws AuthenticationException {
-		 //String name = authentication.getName();
-	     //String password = authentication.getCredentials().toString();
+		Object userName = authentication.getPrincipal();
+		Object userPwd = authentication.getCredentials();
 
-		 Object userId = authentication.getPrincipal();
-		 Object userPwd = authentication.getCredentials();
+		UsernamePasswordAuthenticationToken upat = null;
+		ExtendUser user = null;
+		UserModel userModel = null;
 
-		 logger.info("This is the first check");
-		 boolean blnCheck = false;
-		 // check
+		try {
+			logger.info("UserDetailsService >> userName >1> " + userName);
 
-//		 // Check Authentication, Authorization
-//		 if(!blnCheck) {
-//			 throw new BadCredentialsException("Username does not match for " + principal);
-//		 }
+			/////////////// You can make another authentication/////////////
 
-		 //Collection<SimpleGrantedAuthority> roles = new ArrayList<SimpleGrantedAuthority>();
-		 //roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+			userModel = new UserModel();
+			userModel.setUserId("testID");
+			userModel.setUserName("admin");
+			userModel.setUserPwd("test");
+			if(userName.equals("admin")) {
+				userModel.setUserStatus("1");				
+			} else {
+				userModel.setUserStatus("2");				
+			}
 
-		 // return new UsernamePasswordAuthenticationToken(principal, password, null);
-		 return null;
+	        Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+	        // For java1.6
+	        switch(Integer.valueOf(userModel.getUserStatus())) {
+	            case 1:
+	                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+	            break;
+	            default:
+	                authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+	            break;
+	        }
+
+	        boolean enabled = true;
+	        boolean accountNonExpired = true;
+	        boolean credentialsNonExpired = true;
+	        boolean accountNonLocked = true;
+
+	        user = new ExtendUser(userModel.getUserId(),  passwordEncoder.encodePassword("test", null), enabled, 
+	        		accountNonExpired, credentialsNonExpired, accountNonLocked, authorities, userModel);
+
+	        upat = new UsernamePasswordAuthenticationToken(user, passwordEncoder.encodePassword("test", null), authorities);
+	        upat.setDetails(authentication.getDetails());
+
+		} catch (Exception e) {
+			logger.error("Select error, userId={}, userStatus={}", userModel.getUserId(), userModel.getUserStatus());
+			logger.error("Exception >> ", e);
+		}
+
+			 return upat;
 	}
 
 	@Override
